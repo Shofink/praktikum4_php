11d76feea9677b34a585be86d09d877b8e808063
Shofink <91359694+Shofink@users.noreply.github.com>
2022-03-22 23:32:09 +0700

    Add files via upload

---
+++ persegipanjang.php
@@ -0,0 +1,9 @@
+<?php
+ require_once "praktikum4.php"
+ $Persegipanjang1 = new PersegiPanjang( 5,4 );
+ $Persegipanjang2 = new PersegiPanjang( 9,8 );
+ echo "<br/>Luas Persegi Panjang I ".$lingkar1->Luas();
+ echo "<br>Luas Persegi Panjang II ".$lingkar2->Luas();
+ echo "<br>Keliling Persegi Panjang I ".$lingkar1->Keliling();
+ echo "<br>Keliling Persegi Panjang II ".$lingkar2->Keliling();
+?>
